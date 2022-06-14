e4636bd482734541c4ae499cfe8fd6f092f6073b
TypicalUsername-ai <62468129+TypicalUsername-ai@users.noreply.github.com>
2022-06-14 15:44:42 +0200

    final changes - all test should pass, both on vectors and 2D array

--- include/imqueue.hpp
+++ include/imqueue.hpp
@@ -5,6 +5,7 @@
 #include "improc.hpp"
 #include <map>
 #include <iostream>
+#include <string>
 #ifndef IMAGE_PROCESSOR__SKELETON_IMQUEUE_HPP
 #define IMAGE_PROCESSOR__SKELETON_IMQUEUE_HPP
 
@@ -12,18 +13,9 @@
 class TaskData{
 public:
     std::filesystem::path input_filepath;
-    std::filesystem::path output_dir;
     std::function<Image(Image)> processing_function;
-    void execute_task(){ //FIXME refactor into a inside function for taskqueue (possibly throw into a cpp file)
-        Image pre_exec = load_bitmap(input_filepath.string());
-        Image post_exec = processing_function(pre_exec);
-        if(!exists(output_dir)){
-            create_directory(output_dir);
-        }
-        save_bitmap(output_dir.string(), post_exec);
-
-    };
-
+    std::filesystem::path output_dir;
+    void execute_task(std::string _id);
 };
 
 using queue_type = std::map<std::string, TaskData>;
@@ -37,12 +29,7 @@ public:
     void add_task(std::string task_id, TaskData task){
         _queue.insert({task_id , task});
     };
-    void execute_all(){
-        for(auto task : _queue){
-            std::cout << "executing task: " << task.first << std::endl;
-            //TODO add execution
-        }
-    };
+    void execute_all();
     const queue_type& get_all_tasks(){ return _queue; }
 };
 
--- src/imqueue.cpp
+++ src/imqueue.cpp
@@ -1,4 +1,30 @@
 //
 // Created by mdomalew on 11.05.2022.
 //
+#include "imqueue.hpp"
+#include "improc.hpp"
 
+void TaskQueue::execute_all() {
+    for(auto task : _queue){
+        std::cout << "executing task: " << task.first << std::endl;
+        task.second.execute_task(task.first);
+    }
+}
+
+void TaskData::execute_task(std::string _id) {
+    Image pre_exec = load_bitmap(input_filepath.string());
+    Image post_exec = processing_function(pre_exec);
+    if(!exists(output_dir)){
+        bool a = create_directory(output_dir);
+        std::cout << a << std::endl;
+    }
+    std::string path_file = output_dir.append(input_filepath.filename().string()).string();
+    path_file = path_file.substr(0, path_file.length() - 4) + "__" + _id + input_filepath.extension().string();
+    std::cout << "saving to: " << path_file << std::endl;
+    int exit = save_bitmap( path_file, post_exec);
+    if(exit == EXIT_FAILURE){
+        std::cout << "saving failed!" << std::endl;
+    }
+    std::cout << "saved in:\n" << std::filesystem::absolute(output_dir).string() << std::endl;
+
+};
